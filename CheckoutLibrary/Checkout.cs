26bf7edf7b6a27e608f0ec73bd10d6973ffae7a9
awais <awais@awais.com>
2021-06-20 20:39:00 +0100

    Added test case where an item is added twice

--- CheckoutLibrary/Checkout.cs
+++ CheckoutLibrary/Checkout.cs
@@ -4,6 +4,7 @@ using CheckoutLibrary.Validators;
 using CheckoutRepositories.Items;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CheckoutLibrary
@@ -29,13 +30,22 @@ namespace CheckoutLibrary
         public void Scan(string item)
         {
             var repositoryItem = _itemValidator.ValidateItem(item);
-            _basketItems.Add(new BasketItem
+            AddItemToBasket(item, repositoryItem);
+        }
+
+        private void AddItemToBasket(string item, Item repositoryItem)
+        {
+            var basketItem = _basketItems.FirstOrDefault(_ => _.Sku == item) ?? new BasketItem
             {
                 Sku = repositoryItem.Sku,
-                Qty = 1,
                 UnitPrice = repositoryItem.UnitPrice,
                 SpecialPrice = repositoryItem.SpecialPrice
-            }) ;
+            };
+
+            basketItem.Qty++;
+
+            if(basketItem.Qty == 1)
+                _basketItems.Add(basketItem);
         }
     }
 }
--- CheckoutLibraryTests/CheckoutTests.cs
+++ CheckoutLibraryTests/CheckoutTests.cs
@@ -52,5 +52,40 @@ namespace CheckoutLibraryTests
             checkout.BasketItems[0].Qty.Should().Be(1);
             itemsRepository.Verify();
         }
+
+        [TestMethod]
+        public void Scan_When_AValidItemIsScannedTwice_Then_ItemQuantityIsUpdatedForCheckout()
+        {
+            var itemsRepository = new Mock<IItemsRepository>();
+            var itemValidator = new ItemValidator(itemsRepository.Object);
+            var checkout = new Checkout(itemValidator);
+            var item = new Item
+            {
+                Sku = "ValidItem",
+                UnitPrice = 1.0M,
+                SpecialPrice = "3 for 2"
+            };
+
+            var basketItem = new BasketItem
+            {
+                Sku = "ValidItem",
+                UnitPrice = 1.0M,
+                Qty = 1,
+                SpecialPrice = "3 for 2"
+            };
+
+            checkout.BasketItems.Add(basketItem);
+
+            itemsRepository.Setup(_ => _.GetItemBySKU("ValidItem")).Returns(item).Verifiable();
+
+            checkout.Scan("ValidItem");
+
+            checkout.BasketItems.Should().NotBeEmpty();
+            checkout.BasketItems[0].Sku.Should().Be(item.Sku);
+            checkout.BasketItems[0].UnitPrice.Should().Be(item.UnitPrice);
+            checkout.BasketItems[0].SpecialPrice.Should().Be(item.SpecialPrice);
+            checkout.BasketItems[0].Qty.Should().Be(2);
+            itemsRepository.Verify();
+        }
     }
 }
